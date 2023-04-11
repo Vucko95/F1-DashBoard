c93f23515bc73a38aa81843705fc652c0ad793de
Sandro Sikic <sandro.sikic@gmail.com>
2023-04-11 19:48:39 +0200

    fix: clean up, optional year query

--- backend/app/routers/drivers.py
+++ backend/app/routers/drivers.py
@@ -1,107 +1,85 @@
+from datetime import datetime
 from fastapi import APIRouter
-import json
-from settings.config import *
-import requests
-import asyncio
-import aiohttp
 from pydantic import BaseModel
+from settings.config import *
+import aiohttp
+import asyncio
+import requests
 
 router = APIRouter()
 
 
+@router.get("/drivers")
+def get_best_players(year: str = None):
+    if not year:
+        year = datetime.now().year
 
+    drivers_url = f"https://ergast.com/api/f1/{year}/drivers.json"
+    response = requests.get(url = drivers_url).json()
 
-@router.get("/players")
-def get_best_players():
-    
-    
-    response = requests.get(url = f1_base_url_plus_drivers_2023)
-    f1_base_url_plus_drivers_2023_json = response.json()
-    list_of_drivers = f1_base_url_plus_drivers_2023_json['MRData']['DriverTable']['Drivers']
-    print(f1_base_url_plus_drivers_2023_json)
+    list_of_drivers = response['MRData']['DriverTable']['Drivers']
+
     drivers = []
     for driver in list_of_drivers:
-        drivers.append({'driverId' : driver['driverId'],'givenName' : driver['givenName'],'familyName' : driver['familyName'],'permanentNumber' : driver['permanentNumber'], })
-    print(drivers)
+        drivers.append({
+            'driverId' : driver['driverId'],
+            'givenName' : driver['givenName'],
+            'familyName' : driver['familyName'],
+            'permanentNumber' : driver['permanentNumber'] 
+        })
+
     return drivers
 
 
-@router.post("/year")
-def get_base_player_info(data: dict):
-    year = data['year']
-    f1_base_url  = 'https://ergast.com/api/f1/'
-    f1_drivers_by_year = f'{f1_base_url}{year}/drivers.json'
-    response = requests.get(url = f1_drivers_by_year)
-    f1_drivers_by_year_json = response.json()
-    list_of_drivers = f1_drivers_by_year_json['MRData']['DriverTable']['Drivers']
-    # print(f1_drivers_by_year_json)
+@router.get("/drivers/{driver_id}")
+def get_drivers_results(driver_id: str, year: str = None):
+    if not year:
+        year = datetime.now().year
+    
+    driver_url  = f'http://ergast.com/api/f1/{year}/drivers/{driver_id}/results.json'
+
+    races_in_year = requests.get(url = driver_url).json()
+
+    racesList = races_in_year['MRData']['RaceTable']['Races']
+
+    races = []
+    for race in racesList:
+        races.append({
+            'circuitId': race['Circuit']['circuitId'],
+            'position' : race['Results'][0]['position'],
+            'raceName' : race['raceName'],
+            'country' : race['Circuit']['Location']['country'],
+        })
+
+    return races
+
+
+@router.get("/driver-standings")
+def get_base_player_info(year: str = None):
+    if not year:
+        year = datetime.now().year
+
+    driver_standing_url = f'https://ergast.com/api/f1/{year}/driverStandings.json'
+
+    driver_standings = requests.get(url = driver_standing_url).json()
+
+    list_of_drivers = driver_standings['MRData']["StandingsTable"]["StandingsLists"][0]["DriverStandings"]
+
     drivers = []
-    for driver in list_of_drivers:
-        drivers.append({'driverId' : driver['driverId'],'givenName' : driver['givenName'],'familyName' : driver['familyName'],'permanentNumber' : driver['permanentNumber'], })
-        # drivers.append({'driverId' : driver['driverId']})
-    # print(drivers)
-    return drivers
-
-
-
-# http://ergast.com/api/f1/drivers
-
-
-
-@router.get("/test")
-def get_best_players():
-    test_url = 'http://ergast.com/api/f1/drivers/results.json'
-    response = requests.get(url = test_url)
-    response_json = response.json()
-    print(response_json)
-    # list_of_drivers = f1_base_url_plus_drivers_2023_json['MRData']['DriverTable']['Drivers']
-    # print(f1_base_url_plus_drivers_2023_json)
-    # drivers = []
-    # for driver in list_of_drivers:
-    #     drivers.append({'driverId' : driver['driverId'],'givenName' : driver['givenName'],'familyName' : driver['familyName'],'permanentNumber' : driver['permanentNumber'], })
-    # print(drivers)
-    # return drivers
-
-# https://ergast.com/api/f1/2018/driverStandings
-# @router.post("/year/driverstandings")
-# def get_base_player_info(data: dict):
-#     year = data['year']
-#     f1_base_url  = 'https://ergast.com/api/f1/'
-#     f1_drivers_by_year = f'{f1_base_url}{year}/drivers.json'
-#     response = requests.get(url = f1_drivers_by_year)
-#     f1_drivers_by_year_json = response.json()
-#     list_of_drivers = f1_drivers_by_year_json['MRData']['DriverTable']['Drivers']
-#     # print(f1_drivers_by_year_json)
-#     drivers = []
-#     for driver in list_of_drivers:
-#         drivers.append({'driverId' : driver['driverId'],'givenName' : driver['givenName'],'familyName' : driver['familyName'],'permanentNumber' : driver['permanentNumber'], })
-#     # print(drivers)
-#     return drivers
-
-
-# https://ergast.com/api/f1/2018/constructorStandings
-@router.post("/year/driverstandings")
-def get_base_player_info(data: dict):
-    year = data['year']
-    f1_drivers_by_year = f'{f1_base_url}{year}/driverStandings.json'
-    response = requests.get(url = f1_drivers_by_year)
-    f1_driver_standings_by_year = response.json()
-    list_of_drivers = f1_driver_standings_by_year['MRData']["StandingsTable"]["StandingsLists"][0]["DriverStandings"]
-    # drivers = []
-    drivers = []
-
     for driver_standing in list_of_drivers:
         driver = driver_standing['Driver']
-        driverstats = driver_standing
         constructor_info = driver_standing['Constructors'][0]
 
-        drivers.append({'driverId': driver['driverId'],
-                        'constructor_id' : constructor_info['constructorId'],
-                        'constructor' : constructor_info['name'],
-                        'points' : driverstats['points'],
-                        'givenName': driver['givenName'],
-                        'familyName': driver['familyName'],
-                        'permanentNumber': driver['permanentNumber']})
+        drivers.append({
+            'driverId': driver['driverId'],
+            'constructor_id' : constructor_info['constructorId'],
+            'constructor' : constructor_info['name'],
+            'points' : driver_standing['points'],
+            'givenName': driver['givenName'],
+            'familyName': driver['familyName'],
+            'permanentNumber': driver['permanentNumber']
+        })
+
     return drivers
 
 
@@ -249,3 +227,39 @@ async def driver_points(year: Year):
     # CACHE
     cache[year.year] = result
     return result
+    
+    
+    
+    
+    
+
+# http://ergast.com/api/f1/drivers
+@router.get("/test")
+def get_best_players():
+    test_url = 'http://ergast.com/api/f1/drivers/results.json'
+    response = requests.get(url = test_url)
+    response_json = response.json()
+    print(response_json)
+    # list_of_drivers = f1_base_url_plus_drivers_2023_json['MRData']['DriverTable']['Drivers']
+    # print(f1_base_url_plus_drivers_2023_json)
+    # drivers = []
+    # for driver in list_of_drivers:
+    #     drivers.append({'driverId' : driver['driverId'],'givenName' : driver['givenName'],'familyName' : driver['familyName'],'permanentNumber' : driver['permanentNumber'], })
+    # print(drivers)
+    # return drivers
+
+# https://ergast.com/api/f1/2018/driverStandings
+# @router.post("/year/driverstandings")
+# def get_base_player_info(data: dict):
+#     year = data['year']
+#     f1_base_url  = 'https://ergast.com/api/f1/'
+#     f1_drivers_by_year = f'{f1_base_url}{year}/drivers.json'
+#     response = requests.get(url = f1_drivers_by_year)
+#     f1_drivers_by_year_json = response.json()
+#     list_of_drivers = f1_drivers_by_year_json['MRData']['DriverTable']['Drivers']
+#     # print(f1_drivers_by_year_json)
+#     drivers = []
+#     for driver in list_of_drivers:
+#         drivers.append({'driverId' : driver['driverId'],'givenName' : driver['givenName'],'familyName' : driver['familyName'],'permanentNumber' : driver['permanentNumber'], })
+#     # print(drivers)
+#     return drivers

d40451d5023aba5973ac4ae5e70af8ad33b12c4f
Sandro Sikic <sandro.sikic@gmail.com>
2023-04-11 19:49:21 +0200

    fix: removed unused imports, comments, print

--- backend/app/routers/news.py
+++ backend/app/routers/news.py
@@ -1,22 +1,23 @@
 from fastapi import APIRouter
-import json, feedparser
 from settings.config import *
-import requests
+import feedparser
+
 router = APIRouter()
-
-
-
 
 @router.get("/news")
 def get_best_players():
     url = "https://feeds.bbci.co.uk/sport/formula1/rss.xml"
+
     feed = feedparser.parse(url)
 
     news_feed = []
-    # for entry in feed['entries']:
-    # LIMIT 8
+
     for entry in feed['entries'][:6]:
-        news_feed.append({'articleTitle' :entry.title,'articleSummary' :entry.summary,'articleLink' :entry.link,'articlepublished' :entry.published  })
-            # drivers.append({'driverId' : driver['driverId'],'givenName' : driver['givenName'],'familyName'published : driver['familyName'],'permanentNumber' : driver['permanentNumber'], })
-    print(news_feed)
+        news_feed.append({
+            'articleTitle' :entry.title,
+            'articleSummary' :entry.summary,
+            'articleLink' :entry.link,
+            'articlepublished' :entry.published  
+        })
+          
     return news_feed
